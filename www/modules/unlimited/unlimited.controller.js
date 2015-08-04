b7e2a5afa9a58a4f06991dd26cd5e7eb31816824
Kiong Kiong <kiong90@gmail.com>
2015-08-05 06:05:54 +0800

    Formatting of codes

--- www/modules/unlimited/unlimited.controller.js
+++ www/modules/unlimited/unlimited.controller.js
@@ -1,102 +1,103 @@
-(function () {
-	angular.module("Unlimited")
-		.controller("unlimitedController", unlimitedController);
+(function() {
+    angular.module("Unlimited")
+        .controller("unlimitedController", unlimitedController);
 
-	unlimitedController.$inject = ['randomService', '$interval'];
-	function unlimitedController (randomService, $interval) {
-		var vm= this;
-		vm.compareNumber = compareNumber;
-		vm.holdIncreaseNo = holdIncreaseNo;
-		vm.stopIncrease = stopIncrease;
+    unlimitedController.$inject = ['randomService', '$interval'];
 
-		vm.resultArray = [];
-		vm.numberInput = {};
-		var tryNumber = 0;
-		var oriNumberInput = {
-			one: 0,
-			two: 0,
-			three: 0,
-			four: 0
-		};
-		var result = {
-			inputNumber: "",
-			tryNo: 0,
-			correctNumberNPlaceCount: 0,
-			correctNumberNWrongPlaceCount: 0
-		};
-		// Format of number generated will be same as vm.numberInput
-		var numberGenerated = {};
-		var promise;
+    function unlimitedController(randomService, $interval) {
+        var vm = this;
+        vm.compareNumber = compareNumber;
+        vm.holdIncreaseNo = holdIncreaseNo;
+        vm.stopIncrease = stopIncrease;
 
-		/* Services */
-		vm.randomService = randomService;
+        vm.resultArray = [];
+        vm.numberInput = {};
+        var tryNumber = 0;
+        var oriNumberInput = {
+            one: 0,
+            two: 0,
+            three: 0,
+            four: 0
+        };
+        var result = {
+            inputNumber: "",
+            tryNo: 0,
+            correctNumberNPlaceCount: 0,
+            correctNumberNWrongPlaceCount: 0
+        };
+        // Format of number generated will be same as vm.numberInput
+        var numberGenerated = {};
+        var promise;
 
-		/* Public Methods */
-		function stopIncrease() {
-			$interval.cancel(promise);
-		}
+        /* Services */
+        vm.randomService = randomService;
 
-		function holdIncreaseNo(value) {
-			promise = $interval(function () { 
-				if(vm.numberInput[value] < 9) {
-					vm.numberInput[value] += 1;
-				} else {
-					vm.numberInput[value] = 0;
-				}
-			}, 100);
-		}
+        /* Public Methods */
+        function stopIncrease() {
+            $interval.cancel(promise);
+        }
 
-		function compareNumber() {
-			var inputObj = {},
-				correctNumberNPlaceCount = 0,
-				correctNumberNWrongPlaceCount = 0,
-				tempArr = [];
+        function holdIncreaseNo(value) {
+            promise = $interval(function() {
+                if (vm.numberInput[value] < 9) {
+                    vm.numberInput[value] += 1;
+                } else {
+                    vm.numberInput[value] = 0;
+                }
+            }, 100);
+        }
 
-			angular.copy(vm.resultArray, tempArr);
-			angular.copy(vm.numberInput, inputObj);
+        function compareNumber() {
+            var inputObj = {},
+                correctNumberNPlaceCount = 0,
+                correctNumberNWrongPlaceCount = 0,
+                tempArr = [];
 
-			clearInput();
+            angular.copy(vm.resultArray, tempArr);
+            angular.copy(vm.numberInput, inputObj);
 
-			for (var key1 in inputObj) {
-				if (inputObj.hasOwnProperty(key1)) {
-					if(inputObj[key1] == numberGenerated[key1]) {
-						correctNumberNPlaceCount++;
-					} else {
-						for(var key2 in numberGenerated){
-							if((numberGenerated.hasOwnProperty(key2)) && (inputObj[key1] == numberGenerated[key1])){
-								correctNumberNWrongPlaceCount++;
-								break;
-							}
-						}
-					}
-				}
-			}
-			result.tryNo = ++tryNumber;
-			result.inputNumber = inputObj.one + "" + inputObj.two + inputObj.three + inputObj.four;
-			result.correctNumberNPlaceCount = correctNumberNPlaceCount;
-			result.correctNumberNWrongPlaceCount = correctNumberNWrongPlaceCount;
-			tempArr.push(result);
-			angular.copy(tempArr, vm.resultArray);
-		}
+            clearInput();
 
-		/* Private Methods */
-		function clearInput() {
-			angular.copy(oriNumberInput,vm.numberInput);
-		}
+            for (var key1 in inputObj) {
+                if (inputObj.hasOwnProperty(key1)) {
+                    if (inputObj[key1] == numberGenerated[key1]) {
+                        correctNumberNPlaceCount++;
+                    } else {
+                        for (var key2 in numberGenerated) {
+                            if ((numberGenerated.hasOwnProperty(key2)) && (inputObj[key1] == numberGenerated[key1])) {
+                                correctNumberNWrongPlaceCount++;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            result.tryNo = ++tryNumber;
+            result.inputNumber = inputObj.one + "" + inputObj.two + inputObj.three + inputObj.four;
+            result.correctNumberNPlaceCount = correctNumberNPlaceCount;
+            result.correctNumberNWrongPlaceCount = correctNumberNWrongPlaceCount;
+            tempArr.push(result);
+            angular.copy(tempArr, vm.resultArray);
+        }
 
-		function init() {
-			vm.randomService.getRandomNumberAsString().then(function (rs){
-				if(rs == undefined || rs == null || rs == "") {
-					throw new Error("rs is undefined or null: "+rs);
-				} else {
-					angular.copy(rs, numberGenerated);
-				}
-			}, function (err) {
-				throw new Error("Error getting random number: "+err);
-			});
-			angular.copy(oriNumberInput,vm.numberInput);
-		}
+        /* Private Methods */
+        function clearInput() {
+            angular.copy(oriNumberInput, vm.numberInput);
+        }
 
-		init();
-	}
+        function init() {
+            vm.randomService.getRandomNumberAsString().then(function(rs) {
+                if (rs == undefined || rs == null || rs == "") {
+                    throw new Error("rs is undefined or null: " + rs);
+                } else {
+                    angular.copy(rs, numberGenerated);
+                }
+            }, function(err) {
+                throw new Error("Error getting random number: " + err);
+            });
+            angular.copy(oriNumberInput, vm.numberInput);
+        }
+
+        init();
+    }
 })();
